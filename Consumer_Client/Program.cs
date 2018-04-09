1fe17fcd5cdf546443f1db23227e21aa0df1702a
yu <yuy@longjubank.com>
2018-04-09 17:51:59 +0800

    持久

--- Consumer_Client/Program.cs
+++ Consumer_Client/Program.cs
@@ -20,11 +20,18 @@ namespace Consumer_Client
             {
                 using (var channel = conn.CreateModel())
                 {
-                    channel.QueueDeclare("hello", false, false, false, null);
+                    bool durable = true;
+                    channel.QueueDeclare("hello", durable, false, false, null);
+                    #region 公平分发
+                    //你可能会注意到，消息的分发可能并没有如我们想要的那样公平分配。比如，对于两个工作者。当奇数个消息的任务比较重，但是偶数个消息任务比较轻时，奇数个工作者始终处理忙碌状态，而偶数个工作者始终处理空闲状态。但是RabbitMQ并不知道这些，他仍然会平均依次的分发消息。
+                    //了改变这一状态，我们可以使用basicQos方法，设置perfetchCount = 1 。这样就告诉RabbitMQ 不要在同一时间给一个工作者发送多于1个的消息，或者换句话说。在一个工作者还在处理消息，并且没有响应消息之前，不要给他分发新的消息。相反，将这条新的消息发送给下一个不那么忙碌的工作者。
+                    //channel.BasicQos(0, 1, false);
+                    #endregion
+
                     var consumer = new EventingBasicConsumer(channel);
 
                     channel.BasicConsume("hello", false, consumer);
-
+                    
                     consumer.Received += (model, ea) =>
                     {
                         var body = ea.Body;
--- Producer_Client/Program.cs
+++ Producer_Client/Program.cs
@@ -31,12 +31,17 @@ namespace Producer_Client
                 //消息是以二进制数组的形式传输的，所以如果消息是实体对象的话，需要序列化和然后转化为二进制数组。
                 using (var channel = conn.CreateModel())
                 {
-                    channel.QueueDeclare("hello", false, false, false, null);
+                    //(1)保证队列和消息都是持久化的。
+                    //queueDeclare 这个改动需要在发送端和接收端同时设置。
+                    bool durable = true;
+                    channel.QueueDeclare("hello", durable, false, false, null);
+
                     string message = "hello world1 w";
                     var body = System.Text.Encoding.UTF8.GetBytes(message);
                     //To publish a message to an exchange, use IModel.BasicPublish as follows:
                     // channel.BasicPublish(exchangeName, routingKey, null, body);
 
+                    #region properties
                     //For fine control, you can use overloaded variants to specify the mandatory flag, or specify messages properties:
                     IBasicProperties properties = channel.CreateBasicProperties();
                     properties.ContentType = "text/plain";
@@ -49,8 +54,13 @@ namespace Producer_Client
                     //Code sample below sets a message expiration
                     properties.Expiration = "36000";
 
+                    //(2)保证消息也是持久化的
+                    //需要注意的是，将消息设置为持久化并不能完全保证消息不丢失。虽然他告诉RabbitMQ将消息保存到磁盘上，但是在RabbitMQ接收到消息和将其保存到磁盘上这之间仍然有一个小的时间窗口。 RabbitMQ 可能只是将消息保存到了缓存中，并没有将其写入到磁盘上。持久化是不能够一定保证的，但是对于一个简单任务队列来说已经足够。如果需要消息队列持久化的强保证，可以使用publisher confirms
+                    properties.Persistent = true;
+                    #endregion
+                     
                     channel.BasicPublish(exchangeName, routingKey, properties, body);
-
+                     
                     Console.WriteLine("已发送：{0}", message);
 
                     while (true)
